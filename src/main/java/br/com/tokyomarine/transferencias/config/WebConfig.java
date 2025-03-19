108f79514b317c6850008295e577fe341da6264f
Lucas Coelho <49642902+lucasCoelho245@users.noreply.github.com>
2025-03-19 00:42:54 -0300

    Update WebConfig.java

--- src/main/java/br/com/tokyomarine/transferencias/config/WebConfig.java
+++ src/main/java/br/com/tokyomarine/transferencias/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("http://localhost:4200", "https://frontend-transferencias.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
